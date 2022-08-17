cf7754de8e4c113ded0b89823489e315494cd7b9
HectorLuevano <hector.luevano@jaak.ai>
2022-08-16 19:58:31 -0500

    feat: adding method to open keyboard when first fragment is resumed

--- app/src/main/java/com/hector/melichallenge/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/hector/melichallenge/presentation/search/SearchFragment.kt
@@ -1,10 +1,12 @@
 package com.hector.melichallenge.presentation.search
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -28,6 +30,14 @@ class SearchFragment : Fragment() {
         setUpTextChangeListener()
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.viewSearch.searchView.requestFocus()
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED,0)
     }
 
     private fun setUpTextChangeListener() {
